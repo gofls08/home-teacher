aaa75f4db1a230877695fc456a177bd7e292c1be
Haerin <beal2002@naver.com>
2023-06-10 16:22:42 +0900

    init

---
+++ src/routes/log-in/+page.svelte
@@ -0,0 +1,32 @@
+<div class="log-in">
+	<div class="name">Home Teacher</div>
+	<button>보내기</button>
+	
+</div>
+
+
+<style>
+	.name {
+		font-size: 50px;
+		font-weight: bold;
+		margin-bottom: 50px;
+	}
+	button{
+		display: block;
+		border: 1px solid rgb(132, 132, 216);
+		background-color: rgb(132, 132, 216);
+		border-radius: 30px;
+		height: 56px;
+		width: 341.6px;
+		padding: 0 20px;
+		text-align: center;
+		left: 48%;
+		margin-bottom: 5%;
+	}
+	.log-in {
+		position: absolute;
+		top: 30%;
+		left: 40%;
+	}
+	
+</style>
