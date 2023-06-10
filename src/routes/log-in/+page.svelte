e80db7b377c6596882a0551d080ba5f579f510f5
Haerin <beal2002@naver.com>
2023-06-10 17:03:20 +0900

    init

--- src/routes/log-in/+page.svelte
+++ src/routes/log-in/+page.svelte
@@ -1,32 +1,49 @@
-<div class="log-in">
-	<div class="name">Home Teacher</div>
-	<button>보내기</button>
+<script lang="ts">
+	 import { onMount } from "svelte";
+
+	 function main(){
+		location.href = "/main";
+	 }
 	
+</script>
+<div id="container">
+	<div class="log-in">
+		<div class="name">Home Teacher</div>
+		<button on:click={main}> Sign in with Google </button>
+	</div>
 </div>
 
-
 <style>
+	#container{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width:100%;
+		height:100vh;
+		overflow: hidden;
+	}
+	.log-in{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 	.name {
 		font-size: 50px;
 		font-weight: bold;
 		margin-bottom: 50px;
+		text-align: center;
 	}
-	button{
+	button {
 		display: block;
 		border: 1px solid rgb(132, 132, 216);
 		background-color: rgb(132, 132, 216);
 		border-radius: 30px;
 		height: 56px;
-		width: 341.6px;
+		width: 300px;
 		padding: 0 20px;
 		text-align: center;
-		left: 48%;
-		margin-bottom: 5%;
-	}
-	.log-in {
-		position: absolute;
-		top: 30%;
-		left: 40%;
+		color: white;
+		font-size: 15px;
 	}
 	
 </style>
